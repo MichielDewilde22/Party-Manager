e769df5963a0d118dd5e5dd7f773a09ab850183d
MichielDewilde22 <37221124+MichielDewilde22@users.noreply.github.com>
2018-05-10 11:53:10 +0200

    Look & feel

--- src/partymanager/PartyManager.java
+++ src/partymanager/PartyManager.java
@@ -10,6 +10,22 @@ public class PartyManager {
     
 	public static void main(String args[])
         {
+            try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            } catch (ClassNotFoundException ex) {
+                java.util.logging.Logger.getLogger(User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
             chooseParty choose = new chooseParty();
             choose.setVisible(true);
         }
